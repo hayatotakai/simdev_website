9c5becccc79dd041f4957415530cdd2a4a44a1c4
haytaotakai <hatakai.us@gmail.com>
2026-02-27 15:10:31 -0500

    Initial commit (without large binaries)

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,4 +2,8 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+	site: 'https://hayatotakai.github.io',
+	base: '/simdev_website',
+	output: 'static',
+});
--- src/config/videos.ts
+++
@@ -1,3 +0,0 @@
-// export const videos = {
-//   airx2: 'https://3e320e2895c3dd99167461441328d5be.r2.cloudflarestorage.com/videos/airx2.mp4',
-// };
